f4a910fe28d724de371bbe0ef1ab9ab2dd2a8537
智言 <5679361@qq.com>
2017-02-21 00:12:58 +0800

    对views下index首页进行设计开发

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '智言个人博客' });
 });
 
 module.exports = router;
